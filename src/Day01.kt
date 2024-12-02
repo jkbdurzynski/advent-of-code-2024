435b6879fc78ea594b3cb3896be6505a9daf3fa7
Jakub Durzynski <jkbdurzynski@gmail.com>
2024-12-02 01:30:17 +0100

    Day1 solution for both puzzles

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,20 +1,30 @@
+import kotlin.math.abs
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    fun part1(input: List<String>): Long = input.map {
+        val first = it.substringBefore(" ").toLong()
+        val second = it.substringAfterLast(" ").toLong()
+        first to second
+    }.unzip()
+        .let { (left, right) ->
+            left.sorted().zip(right.sorted())
+                .sumOf { (leftItem, rightItem) -> abs(leftItem - rightItem) }
+        }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    fun part2(input: List<String>): Long = input.map {
+        val first = it.substringBefore(" ").toLong()
+        val second = it.substringAfterLast(" ").toLong()
+        first to second
+    }.unzip()
+        .let { (left, right) ->
+            val occurrences = right.groupingBy { it }.eachCount()
+            left.sumOf { it * (occurrences[it] ?: 0) }
+        }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
 
-    // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+//    val testInput = readInput("Day01_test")
+//    check(part1(testInput) == 1L)
 
-    // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()

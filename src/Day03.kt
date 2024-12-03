72a5c142bdd0f6282b620f1aa8fc13afcf40dc5c
Jakub Durzynski <jkbdurzynski@gmail.com>
2024-12-03 18:30:15 +0100

    Day3; some improvements

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,41 +1,35 @@
 fun main() {
 
 
+    fun multiplyResult(it: MatchResult): Long {
+        check(it.groupValues.size == 3)
+        return it.groupValues[1].toLong() * it.groupValues[2].toLong()
+    }
+
     fun part1(input: List<String>): Long = input.flatMap {
         """mul\((\d{1,3}),(\d{1,3})\)""".toRegex().findAll(it)
-    }.sumOf {
-        check(it.groupValues.size == 3)
-        it.groupValues[1].toLong() * it.groupValues[2].toLong()
-    }
+    }.sumOf { multiplyResult(it) }
 
     fun part2(input: List<String>): Long {
         var enabled = true
-        return input.flatMap {
-            """mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)""".toRegex().findAll(it)
-        }.sumOf {
-            when (it.value) {
-                "do()" -> {
-                    enabled = true
-                    0
-                }
-                "don't()" -> {
-                    enabled = false
-                    0
-                }
-                else -> {
-                    if (enabled) {
-                        check(it.groupValues.size == 3)
-                        it.groupValues[1].toLong() * it.groupValues[2].toLong()
-                    } else {
-                        0
+        val regex = """mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)""".toRegex()
+        return input.flatMap { regex.findAll(it) }
+            .filter {
+                when (it.value) {
+                    "do()" -> {
+                        enabled = true
+                        false
                     }
-                }
-            }
-        }
-    }
 
-//    val testInput = readInput("Day01_test")
-//    check(part1(testInput) == 1L)
+                    "don't()" -> {
+                        enabled = false
+                        false
+                    }
+
+                    else -> enabled
+                }
+            }.sumOf { multiplyResult(it) }
+    }
 
     val input = readInput("Day03")
     part1(input).println()
